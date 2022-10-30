ffbadde5f67c82556f1f8b845e18756d10fba0b0
linhtinhthhd <linhtinhthhd@gmail.com>
2022-10-30 14:57:20 +0700

    ds

--- src/utils/fixCors.js
+++ src/utils/fixCors.js
@@ -4,7 +4,7 @@ export default axios.create({
     baseURL: 'https://cors-anywhere.herokuapp.com/http://localhost:3001',
     headers: {
         'content-type': 'application/json; charset=UTF-8',
-        // 'access-control-allow-origin': '*',
+        'access-control-allow-origin': '*',
         'access-control-expose-headers': '*',
     },
     withCredentials: true,
