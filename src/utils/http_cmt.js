6bd94f09e609292ba254d1f95488e7835d14bde5
linhtinhthhd <linhtinhthhd@gmail.com>
2022-11-08 17:31:24 +0700

    fix

--- src/utils/http_cmt.js
+++ src/utils/http_cmt.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:3001/api/comments/',
+    baseURL: 'https://moviepinklink.herokuapp.com/api/comments/',
     headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': true,
--- src/utils/http_common.js
+++ src/utils/http_common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:3001/api/users',
+    baseURL: 'https://moviepinklink.herokuapp.com/api/users',
     headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': true,
